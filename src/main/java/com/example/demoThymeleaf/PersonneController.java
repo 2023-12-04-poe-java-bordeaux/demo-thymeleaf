44cf7bc4154b92957a244df2b02b70221da07171
jcdominguez <jcd.pro@gmail.com>
2024-02-01 11:26:56 +0100

    trasmission données du @Controller vers la vue Thymeleaf

--- src/main/java/com/example/demoThymeleaf/PersonneController.java
+++ src/main/java/com/example/demoThymeleaf/PersonneController.java
@@ -1,13 +1,18 @@
 package com.example.demoThymeleaf;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonneController {
 
     @GetMapping("hello")
-    public String hello(){
+    public String hello(Model model){
+
+        String prenom = "Jean-Christophe";
+        model.addAttribute("prenom",prenom);
+
         return "hello.html";
     }
 }
